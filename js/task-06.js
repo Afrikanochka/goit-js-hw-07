e798cbbbdf4bd0fb3c7154949de24003115f1243
Afrikanochka <Mtgnatali@gmail.com>
2021-06-15 21:51:36 +0300

    add tasks 6, 8 not finished

--- js/task-06.js
+++ js/task-06.js
@@ -1,3 +1,18 @@
 
 const textElem = document.querySelector('#validation-input');
 const dataLength = textElem.getAttribute("data-length");
+
+const allClasses = (add, remove) => {
+    textElem.classList.add(add);
+    textElem.classList.remove(remove);
+}
+textElem.addEventListener('blur', event => {
+    const input = event.target.value;
+    
+     if (input.length === +dataLength) {
+         allClasses('valid', 'invalid');
+         
+     } else {
+         allClasses('invalid', 'valid');
+  }
+});
---
+++ js/task-08.js
@@ -0,0 +1,37 @@
+const refs = {
+    render: document.querySelector('[data-action="render"]'),
+    destroy: document.querySelector('[data-action="destroy"]'),
+    boxes: document.querySelector('#boxes'),
+    inputControl: document.querySelector('#controls input'),
+};
+refs.render.addEventListener('click', getAmount);
+refs.destroy.addEventListener('click', destroyBoxes);
+
+function getAmount() {
+    const amount = refs.inputControl.value;
+    createBoxes(amount);
+}
+
+function createBoxes(amount) {
+    let size = 30;
+    let arrMarkup = [];
+    for (let i = 0; i < amount; i++) {
+        const newDiv = document.createElement('div');
+        newDiv.style.width = size + 'px';
+        newDiv.style.height = size + 'px';
+        newDiv.classList.add('item');
+        arrMarkup.push(newDiv);
+        size += 10;
+    }
+    refs.boxes.append(...arrMarkup);
+    console.log(arrMarkup);
+}
+function destroyBoxes() {
+  boxes.innerHTML = '';
+}
+
+function random(min, max) {
+    return Math.floor(Math.random() * (max - min + 1));    
+}
+
+console.log(random(0, 255));
